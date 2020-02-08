b405b4678a305ea2e4b108ae159a7111fb3c66bc
Shang_SRant <shangsrant@gmail.com>
2020-02-08 19:22:35 +0800

    V 1.1

---
+++ app/src/main/java/com/shangtianqi/dice/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.shangtianqi.dice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { radomNumber() }
+    }
+
+    private fun radomNumber() {
+        val randomInt = (1..6).random()
+
+        lateinit var diceImage: ImageView
+
+        diceImage = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(drawableResource)
+        textView.text = "点数是：${randomInt}"
+    }
+}
